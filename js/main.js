582d9713f68232af57995f0f5c3fddfb3285e237
valeshan <valeshan@gmail.com>
2017-12-10 19:42:48 +1100

    added other text field option
    added text field option when other is selected

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-alert("this works!");
+$("#name").focus();
+
+
+const jobRole = document.getElementById('title');
+const inputBox = document.getElementById('otheroption');
+
+inputBox.style.display = "none";
+
+const listener = function(){
+  if(jobRole.value == "other"){
+    inputBox.style.display = "block";
+  }else{
+    inputBox.style.display = "none";
+  }
+}
+
+
+jobRole.addEventListener('change',listener);
