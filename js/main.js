d7e9d495bbce49303d91df7f2eb6b47c87b1a096
valeshan <valeshan@gmail.com>
2017-12-11 11:08:16 +1100

    completed t-shirt info section
    fixed hide/show options when either js puns or hearts js is selected.
    also updated names of id in html for the Other textfield, and the function name to showOther.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ $("#name").focus();
 
 //all variable selectors
 const jobRole = document.getElementById('title');
-const inputBox = document.getElementById('otheroption');
+const inputBox = document.getElementById('other-title');
 const design = document.getElementById('design');
 const color = document.getElementById('color');
 
+//hide other-title by default
 inputBox.style.display = "none";
 
-const listener = function(){
+//function to display other-title when other option is selected
+const showOther = function(){
   if(jobRole.value == "other"){
     inputBox.style.display = "block";
   }else{
@@ -16,19 +18,36 @@ const listener = function(){
   }
 }
 
-jobRole.addEventListener('change',listener);
+//event listener for change to jobRole list
+jobRole.addEventListener('change', showOther);
 
 
 //when option is chosen, only show partial selection
 const themeChanger = function(){
   if(design.value == "js puns"){
-    alert("heart you");
+    for (let i= 0; i <= color.length; i++){
+      if (color.options[i].text.endsWith("JS shirt only)")){
+           color.options[i].style.display = "none";
+           color.options[0].selected = true;
+         }
+      else{
+        color.options[i].style.display = "block";
+      }
+       }
   } else if(design.value == "heart js"){
-     if (color.value.endsWith("(JS Puns shirt only)")){
-       alert("sy")
-     };
-
+      for (let i= 0; i <= color.length; i++){
+        if (color.options[i].text.endsWith("(JS Puns shirt only)")){
+             color.options[i].style.display = "none";
+             color.options[3].selected = true;
+        }
+        else{
+          color.options[i].style.display = "block";
+        }
+      }
   }
-}
+  }
+
+
+
 
 design.addEventListener('change', themeChanger);
