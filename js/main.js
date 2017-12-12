a44f2851d1571201d6f01e6a011517101e581ce3
valeshan <valeshan@gmail.com>
2017-12-12 15:07:16 +1100

    completed totalcalc
    -added value attributes
    -appended total span to activities fieldset
    -created totalCalc function
    -set totalCalc to run when change to activities input is made

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-$("#name").focus();
+//*********** VARIABLES ************//
 
 //all variable selectors
 const jobRole = document.getElementById('title');
 const inputBox = document.getElementById('other-title');
 const design = document.getElementById('design');
 const color = document.getElementById('color');
+const activities = document.getElementsByClassName('activities');
+
+
+//*********** FOCUS ************//
+
+$("#name").focus();
+
+
+//************ OTHER TEXTFIELD ***************//
 
 //hide other-title by default
 inputBox.style.display = "none";
@@ -21,6 +30,8 @@ const showOther = function(){
 //event listener for change to jobRole list
 jobRole.addEventListener('change', showOther);
 
+
+//*********** THEME CHANGER ************//
 
 //when option is chosen, only show partial selection
 const themeChanger = function(){
@@ -47,7 +58,38 @@ const themeChanger = function(){
   }
   }
 
-
-
-
 design.addEventListener('change', themeChanger);
+
+
+//************ ACTIVITIES REGISTER ***************//
+
+//*** Run Total ***//
+
+//add value attributes to each checkbox
+$(".activities input[name=all]").attr("value", "200");
+$(".activities input[name=js-frameworks]").attr("value", "100");
+$(".activities input[name=js-libs]").attr("value", "100");
+$(".activities input[name=express]").attr("value", "100");
+$(".activities input[name=node]").attr("value", "100");
+$(".activities input[name=build-tools]").attr("value", "100");
+$(".activities input[name=npm]").attr("value", "100");
+
+//append span element to activities fieldset
+$(".activities").append("<span class='total'></span>");
+
+//create totalCalc that adds cost to total whenever checkbox is checked and updates total span text
+function totalCalc(){
+    let total = 0;
+    $(".activities input[type='checkbox']:checked").each(function(){
+           let cost = $(this).val();
+           total += parseInt(cost);
+    })
+    $('.total').text('$'+total);
+};
+
+//run totalCalc whenever change is made
+$('.activities input').change(()=>{
+    totalCalc();
+});
+
+//*** Event Overlap ***//
